78b564407b7706b5370334766320ba35a4892327
beini <874140704@qq.com>
2017-11-02 17:34:43 +0800

    添加http接口链接

--- app/src/main/java/beini/com/dailyapp/constant/NetConstants.java
+++ app/src/main/java/beini/com/dailyapp/constant/NetConstants.java
@@ -5,7 +5,9 @@ package beini.com.dailyapp.constant;
  */
 
 public class NetConstants {
-    public static String ROOT_URL = "https://10.0.0.40:8443/";
+    public static String ROOT_URL = "http://10.0.0.40:8080/";
+//    public static String ROOT_URL = "https://10.0.0.40:8433/";
+
     //action
     public static String URL_ADD_DAILY = "addDaily";
     public static String URL_REGISTER_USER = "register";
